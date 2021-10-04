a3a736b757293e75c3775fd43cb490b6f387bdf7
Siarhei Kvachonak <s.kvachenok@syberry.com>
2021-10-04 23:29:19 +0300

    task-7: Change authorization

--- import/lambdas/common/handleResponse.js
+++ import/lambdas/common/handleResponse.js
@@ -4,6 +4,7 @@ export const handleResponse = (data, status = 200) => {
       'Content-Type': '*',
       'Access-Control-Allow-Methods': '*',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
     statusCode: status,
     body: data,
